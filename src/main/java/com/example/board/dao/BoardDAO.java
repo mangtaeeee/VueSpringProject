2820ac928a5c153e54bc4094e31421606dfd93d8
mangtaeeee <osp9658@naver.com>
2022-04-20 14:19:04 +0900

    boardList json 으로 가져오는거

--- src/main/java/com/example/board/dao/BoardDAO.java
+++ src/main/java/com/example/board/dao/BoardDAO.java
@@ -2,8 +2,11 @@ package com.example.board.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.example.board.vo.BoardVO;
 
+@Mapper
 public interface BoardDAO {
 	public List<BoardVO> boardList (BoardVO bvo);
 	
--- src/main/java/com/example/board/vo/BoardVO.java
+++ src/main/java/com/example/board/vo/BoardVO.java
@@ -4,12 +4,12 @@ import lombok.Data;
 
 @Data
 public class BoardVO { 			//게시판 테이블 
-	private String mId ; 		//회원 아이디 
-	private int bNum;			//게시판 번호
-	private long bFile; 		//파일
-	private String bTitle; 		//게시판 제목
-	private long bContent; 		//게시판 내용
-	private String bRgedate;	//게시판 등록일
+	private String m_id ; 		//회원 아이디 
+	private int b_num;			//게시판 번호
+	private String b_file; 		//파일
+	private String b_title; 		//게시판 제목
+	private String b_content; 		//게시판 내용
+	private String b_regdate;	//게시판 등록일
 	
 	
 }
