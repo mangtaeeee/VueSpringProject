6e93c290d6657d6e4692d10ee2538bab7709e0bc
mangtaeeee <osp9658@naver.com>
2022-04-20 13:52:06 +0900

    기본설정하기

--- src/main/java/com/example/board/vo/BoardVO.java
+++ src/main/java/com/example/board/vo/BoardVO.java
@@ -1,5 +1,15 @@
 package com.example.board.vo;
 
-public class BoardVO {
+import lombok.Data;
 
+@Data
+public class BoardVO { 			//게시판 테이블 
+	private String mId ; 		//회원 아이디 
+	private int bNum;			//게시판 번호
+	private long bFile; 		//파일
+	private String bTitle; 		//게시판 제목
+	private long bContent; 		//게시판 내용
+	private String bRgedate;	//게시판 등록일
+	
+	
 }
