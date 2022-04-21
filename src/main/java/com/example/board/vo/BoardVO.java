997ea8713c327f401aad28c93599177a9e5211a4
mangtaeeee <osp9658@naver.com>
2022-04-21 11:14:02 +0900

    22.04.21

--- src/main/java/com/example/board/vo/BoardVO.java
+++ src/main/java/com/example/board/vo/BoardVO.java
@@ -8,7 +8,7 @@ public class BoardVO { 			//게시판 테이블
 	private int bNum;			//게시판 번호
 	private String bFile; 		//파일
 	private String bTitle; 		//게시판 제목
-	private String bContent; 		//게시판 내용
+	private String bContent; 	//게시판 내용
 	private String bRegdate;	//게시판 등록일
 	
 	
